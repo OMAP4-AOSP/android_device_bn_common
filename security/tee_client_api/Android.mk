4addd16607f9b4796b64f8102e0f0edefeba7961
Mike J. Chen <mjchen@google.com>
2012-10-16 18:55:15 -0700

    TI mainline's security
    Change-Id: I0cf5da4d3b5653244b8ffe9178d45c3582b48897
    Signed-off-by: Mike J. Chen <mjchen@google.com>

--- security/tee_client_api/Android.mk
+++ security/tee_client_api/Android.mk
@@ -1,4 +1,8 @@
-ifeq ($(TARGET_BOARD_PLATFORM),omap4)
+# Only applicable for OMAP4 and OMAP5 boards.
+# First eliminate OMAP3 and then ensure that this is not used
+# for customer boards
+ifneq ($(TARGET_BOARD_PLATFORM),omap3)
+ifeq ($(findstring omap, $(TARGET_BOARD_PLATFORM)),omap)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -16,10 +20,12 @@ endif
 LOCAL_LDLIBS += -llog
 
 LOCAL_CFLAGS += -DLINUX
+LOCAL_CFLAGS += -DANDROID
 LOCAL_CFLAGS += -I $(LOCAL_PATH)/../tf_sdk/include/
 
 LOCAL_MODULE:= libtee_client_api_driver
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
+endif
 endif
