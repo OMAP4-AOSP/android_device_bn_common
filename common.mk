bd70c31d3cdda1843f95ef103ce501ca42ba2455
D. Andrei Măceș <dmaces@nd.edu>
2016-02-10 15:59:50 -0500

    bn-common: Commonize fstabs & other root files
    Leave control over PRODUCT_COPY_FILES in device trees for now.
    
    Change-Id: Ifba94ec0deebd836bead2ce3cfe06605c1bf1c5c

--- common.mk
+++ common.mk
@@ -37,7 +37,7 @@ endif
 
 # Rootfs
 PRODUCT_COPY_FILES += \
-    $(COMMON_FOLDER)/init.hd-common.rc:/root/init.hd-common.rc \
+    $(COMMON_FOLDER)/root/init.hd-common.rc:/root/init.hd-common.rc \
     $(COMMON_FOLDER)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
     $(COMMON_FOLDER)/recovery/root/sbin/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
 
